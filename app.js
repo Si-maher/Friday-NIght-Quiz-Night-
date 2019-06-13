2f367ab5066e1267d9f020e44d678547a6eed06f
Simon Maher <simon.maher@integrify.io>
2019-06-13 10:15:46 +0300

    Finished updated score section

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -9,10 +10,11 @@ form.addEventListener('submit', e => {
 
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]) {
-            score += 2
+            score += 25
         }
     })
-    console.log(score)
+    result.querySelector('span').textContent = `${score}%`
+    result.classList.remove('d-none')
 
 
 })
